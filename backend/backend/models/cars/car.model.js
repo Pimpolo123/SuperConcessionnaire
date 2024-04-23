e200e3265edf6e104d5b9a3736de3a64c1e03124
JF <jefjef2222@gmail.com>
2024-04-24 00:32:00 +0200

    fix car model

--- backend/backend/models/cars/car.model.js
+++ backend/backend/models/cars/car.model.js
@@ -1,13 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
     const Car = sequelize.define("cars", {
-		make: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
-        model: {
-			type: Sequelize.STRING,
-			allowNull: false,
-		},
         power: {
 			type: Sequelize.INTEGER,
 			allowNull: false,
@@ -16,27 +8,11 @@ module.exports = (sequelize, Sequelize) => {
 			type: Sequelize.INTEGER,
 			allowNull: true,
 		},
-        category: {
-			type: Sequelize.STRING,
-			allowNull: true,
-		},
-        gearboxtype: {
-			type: Sequelize.STRING,
-			allowNull: true,
-		},
         gears: {
 			type: Sequelize.INTEGER,
 			allowNull: true,
-		},
-        drivetrain: {
-			type: Sequelize.STRING,
-			allowNull: true,
-		},
-        fueltype: {
-			type: Sequelize.STRING,
-			allowNull: true,
-		},
+		}
     });
-  
+
     return Car;
 };
